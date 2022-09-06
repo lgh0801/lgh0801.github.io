652ce80b15099353df1a5460f6a09ab0422a0ddb
Hyeok <dlrmsgur4098@gmail.com>
2022-09-06 13:59:41 +0900

    Initial Commit

--- LEEBLOG/gatsby-config.js
+++ LEEBLOG/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `개발 블로그`,
+    description: `저를 표현한 블로그입니다.`,
+    author: `Hyeok`,
+    siteUrl: 'https://lgh0801.github.io/',
   },
   plugins: [
     {
@@ -10,6 +11,13 @@ module.exports = {
       options: {
         isTSX: true,
         allExtensions: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://lgh0801.github.io/',
+        stripQueryString: true,
       },
     },
     `gatsby-plugin-emotion`,
